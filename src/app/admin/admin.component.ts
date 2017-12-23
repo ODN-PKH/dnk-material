c084c4815a6854f421b5103fab20101b5f997449
Sujith Savithry Balan <sujithma1992@gmail.com>
2017-12-23 11:51:24 +0530

    sidenav responsive

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MediaChange, ObservableMedia } from "@angular/flex-layout";
 
 @Component({
   selector: 'dnk-admin',
@@ -7,9 +8,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdminComponent implements OnInit {
 
-  constructor() { }
+	sideNavOpened: boolean = true;
+	sideNavMode: string = 'side';
+  	constructor(private media: ObservableMedia) { }
 
-  ngOnInit() {
-  }
+  	ngOnInit() {
+  		this.toggleView();
+  	}
+  	toggleView() {
+		if (this.media.isActive('gt-md')) {
+            this.sideNavMode = 'side';
+            this.sideNavOpened = true;
+        } else if(this.media.isActive('gt-xs')) {
+            this.sideNavMode = 'side';
+            this.sideNavOpened = false;
+        } else if (this.media.isActive('lt-sm')) {
+            this.sideNavMode = 'side';
+            this.sideNavOpened = false;
+        }
+	}
 
 }
